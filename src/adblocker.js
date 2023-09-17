79470f6c49be0e833b3797156e56b18106c4f1d5
Marcell Perger <marcell.perger.1@gmail.com>
2023-09-17 20:02:27 +0100

    feat: Replace window.open w/ no-newtab version

--- src/adblocker.js
+++ src/adblocker.js
@@ -4,5 +4,14 @@
     if(e.getAttribute("target") == "_blank") e.setAttribute("target", "_self")
     if(e.getAttribute("formtarget") == "_blank") e.setAttribute("formtarget", "_self")
   }
+  // this will only work if the function doesn't keep the ref to the old window.open
+  // but there is no better way to do it as far as I can tell
+  var origOpen = window._origOpen = window.open;
+  window.open = function open(url, target, features) {
+    if(target === '_blank') {
+      target = '_self'
+    }
+    return origOpen(url, target, features);
+  }
   console.log("Hello world");
 })()
