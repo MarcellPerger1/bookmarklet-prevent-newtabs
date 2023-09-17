8216b87670f3b5f05ecad4721c1da0d176b13d00
Marcell Perger <marcell.perger.1@gmail.com>
2023-09-17 19:22:23 +0100

    feat: Add simple case for HTML `target` attribute

--- src/adblocker.js
+++ src/adblocker.js
@@ -1,4 +1,8 @@
 /* eslint-env browser, es2021 */
 (function() {
-    console.log("Hello world");
+  for(const e of document.querySelectorAll('[target="_blank"],[formtarget="_blank"]')) {
+    if(e.getAttribute("target") == "_blank") e.setAttribute("target", "_self")
+    if(e.getAttribute("formtarget") == "_blank") e.setAttribute("formtarget", "_self")
+  }
+  console.log("Hello world");
 })()
